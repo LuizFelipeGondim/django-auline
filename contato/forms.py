b1a3d0692d1b80cdb751cec44b47ea02529383aa
LuizFelipeGondim <luizfelipegondimaraujo@gmail.com>
2020-06-15 18:39:16 -0300

    Mudanças no css dos formulários

--- contato/forms.py
+++ contato/forms.py
@@ -6,4 +6,9 @@ class ContatoForm(ModelForm):
     class Meta:
         model = Contato
         fields = {'nome', 'email', 'conteudo'}
+        widgets = {
+            'nome': forms.TextInput(attrs={'class':'form-control'}),
+            'conteudo': forms.Textarea(attrs={'class':'form-control', "rows":5, "cols":20}),
+            'email': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Adicione um E-mail válido'}),
+        }
         
